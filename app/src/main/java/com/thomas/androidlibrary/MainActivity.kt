fb39e8a4be7edc6580faf11fd4a601dd5b5ed307
Thomas Papapaschos <tpapapaschos@cognity.gr>
2023-05-19 18:47:07 +0300

    Initial commit

---
+++ app/src/main/java/com/thomas/androidlibrary/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.thomas.androidlibrary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.thomas.androidlibrary.ui.theme.AndroidLibraryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidLibraryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("hello")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidLibraryTheme {
+        Greeting("Android")
+    }
+}
---
+++ mytestlibraryhelloworld/src/main/java/com/thomas/mytestlibraryhelloworld/TestLibraryHelloWorld.kt
@@ -0,0 +1,8 @@
+package com.thomas.mytestlibraryhelloworld
+
+class TestLibraryHelloWorld {
+
+    fun getHelloWorldText() : String {
+        return "Hello World from Library!"
+    }
+}
