c3ae51ef44224287e5176555b81a8e3ece5593ce
Thomas Papapaschos <tpapapaschos@cognity.gr>
2023-05-23 11:11:59 +0300

    Added Composable function to test.

--- mytestlibraryhelloworld/src/main/java/com/thomas/mytestlibraryhelloworld/TestLibraryHelloWorld.kt
+++ mytestlibraryhelloworld/src/main/java/com/thomas/mytestlibraryhelloworld/TestLibraryHelloWorld.kt
@@ -1,8 +1,20 @@
 package com.thomas.mytestlibraryhelloworld
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Text
+
 class TestLibraryHelloWorld {
 
     fun getHelloWorldText() : String {
         return "Hello World from Library!"
+    }
+
+    @Composable
+    fun Greeting(text: String, modifier: Modifier = Modifier) {
+        Text(
+            text = text,
+            modifier = modifier
+        )
     }
 }
